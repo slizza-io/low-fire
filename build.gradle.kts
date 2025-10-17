fd988ee334d65570548400362895c9fc58dbc8a4
slizza-io <slizza.io46@gmail.com>
2025-10-17 15:03:59 +0200

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
 
 group = "io.slizza"
-version = providers.environmentVariable("VERSION").getOrElse("1.0.5")
+version = providers.environmentVariable("VERSION").getOrElse("1.0.6")
 
 labyMod {
     defaultPackageName = "io.slizza.lowfire"
